7775d5f95c388f1f42f69a89f98eafa31054a522
IvanHrIvanov <vinko96@abv.bg>
2020-09-26 22:12:23 +0300

    Add

--- 08. Beer Kegs/Program.cs
+++ 08. Beer Kegs/Program.cs
@@ -6,7 +6,33 @@ namespace _08._Beer_Kegs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int input = int.Parse(Console.ReadLine());
+
+            string bigKeg = string.Empty;
+  
+            double maxKeg = double.MinValue;
+
+            for (int i = 0; i < input; i++)
+            {
+                string model = Console.ReadLine();
+
+                double radius = double.Parse(Console.ReadLine());
+
+                int height = int.Parse(Console.ReadLine());
+
+                double sum = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (maxKeg < sum)
+                {
+                    maxKeg = sum;
+                    bigKeg = model;
+
+                }
+
+            }
+
+            Console.WriteLine(bigKeg);
+
         }
     }
 }
---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+
+            int coureses = 0;
+
+            while (one > 0)
+            {
+                one -= two;
+                coureses++;
+
+            }
+            Console.WriteLine(coureses);
+        }
+    }
+}
