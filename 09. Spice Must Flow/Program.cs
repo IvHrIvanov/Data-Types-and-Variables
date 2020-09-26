06a8d92e210c28929ef2b3eef8816a0315a56728
IvanHrIvanov <vinko96@abv.bg>
2020-09-27 01:39:30 +0300

    Add

---
+++ 09. Spice Must Flow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int spicy = int.Parse(Console.ReadLine());
+
+            int workersConsume = 0;
+            int days = 0;
+            
+
+            while (spicy >= 100)
+            {
+                days++;
+                workersConsume += spicy;
+                workersConsume -= 26;                
+                spicy -= 10;              
+            }
+            if (workersConsume >= 26)
+            {
+                workersConsume -= 26;
+
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(workersConsume);
+        }
+    }
+}
